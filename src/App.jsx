306ae9b7a690364058c5b17c35054723bb573a7e
Tomas Brejl <tomas.brejl@gmail.com>
2025-10-18 14:59:01 -0700

    Added barcode reading to backend

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,17 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import reactLogo from './assets/react.svg'
 import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
   const [count, setCount] = useState(0)
+  const [currentTime, setCurrentTime] = useState(0);
+
+  useEffect(() => {
+    fetch('/api/time').then(res => res.json()).then(data => {
+      setCurrentTime(data.time);
+    });
+  }, []);
 
   return (
     <>
@@ -21,6 +28,7 @@ function App() {
         <button onClick={() => setCount((count) => count + 1)}>
           count is {count}
         </button>
+        <p>The current time is {new Date(currentTime * 1000).toLocaleString()}.</p>
         <p>
           Edit <code>src/App.jsx</code> and save to test HMR
         </p>
