5777a164714285a711692d44ec9867ad0b870f38
Tomas Brejl <tomas.brejl@gmail.com>
2025-10-18 18:50:01 -0700

    finished barcode scanning frontend

--- api/api.py
+++ api/api.py
@@ -1,9 +1,11 @@
 import time
 from flask import Flask, jsonify, request
+from flask_cors import CORS, cross_origin
 import requests
 
 app = Flask(__name__)
-FOOD_DATA_KEY = "DEMO_KEY" #Replace with actual key
+CORS(app)
+FOOD_DATA_KEY = "ObAd8jFsex3BZhX7HtE4Ax4KApFezjSkymZdLSH9" #Replace with actual key
 
 
 @app.route('/api/time')
@@ -13,7 +15,13 @@ def get_current_time():
 @app.route('/api/getfood')
 def get_food_info():
   print('DEBUG: Requested barcode data')
+
+  #check in our database if we have the barcode logged
+
+
+  #else, query the api
   query = request.args.get('barcode', 'N/A')
+  page = '1'
   if query == 'N/A':
     query = request.args.get('search', 'N/A')
     page = request.args.get('page', '1')
@@ -24,8 +32,10 @@ def get_food_info():
     response.raise_for_status()
     data = response.json()
   except requests.exceptions.HTTPError as http_err:
+    print(f'DEBUG: HTTP error: {http_err}')
     return jsonify({'error': f'HTTP error occurred: {http_err}'}), 500
   except Exception as err:
+    print(f'DEBUG: Other error: {err}')
     return jsonify({'error': f'Other error occurred: {err}'}), 500
 
   print(f'DEBUG: Sending response {data}')
